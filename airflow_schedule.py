d1741fadcc3331cc96613f560bdbe36b39459b85
Evan Lloyd Flint <evan.lloyd.flint@gmail.com>
2019-05-02 10:30:48 -0400

    updated airflow and aws

---
+++ airflow_schedule.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('skype_bot', default_args=default_args, schedule_interval=timedelta(days=1))
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='run_test',
+    bash_command='test.py',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    retries=3,
+    dag=dag)
+
+# templated_command = """
+#     {% for i in range(5) %}
+#         echo "{{ ds }}"
+#         echo "{{ macros.ds_add(ds, 7)}}"
+#         echo "{{ params.my_param }}"
+#     {% endfor %}
+# """
+
+# t3 = BashOperator(
+#     task_id='templated',
+#     bash_command=templated_command,
+#     params={'my_param': 'Parameter I passed in'},
+#     dag=dag)
+
+t2.set_upstream(t1)
+# t3.set_upstream(t1)
