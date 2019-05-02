d33372fab5d3dbf291a7ffc0ca5b26cefdc7f869
Evan Lloyd Flint <evan.lloyd.flint@gmail.com>
2019-05-02 10:33:28 -0400

    removed hive job scheduler

--- hive_job_scheduler.py
+++
@@ -1,6 +0,0 @@
-from hivebatch import load_hive_data
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-scheduler = BlockingScheduler()
-scheduler.add_job(load_hive_data, 'interval', seconds=60)
-scheduler.start()
